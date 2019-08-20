4ba5de3437c5486ea3a30725e6643cb72ac38196
mduccc <duc2521997@gmail.com>
2019-08-20 21:24:23 +0700

    #06

--- business/v1/Dust.js
+++ business/v1/Dust.js
@@ -7,32 +7,8 @@ module.exports = class Dust {
         this.ValidateToken = new this.validateToken()
         this.admin = require('../../FirebaseAdmin')
         this.db = new this.admin().firestoreDB()
-    }
-
-    async getCityName(city_id) {
-        let result = false
-        await this.db.collection('cities').get()
-            .then(async snapshot => {
-                let ids = await snapshot.docs.map(doc => doc.id)
-                let datas = await snapshot.docs.map(doc => doc.data())
-                for (let i = 0; i < datas.length; i++) {
-                    let id = ids[i]
-                    let data = datas[i]
-                    //console.log('City list =>', data)
-                    if (city_id === id) {
-                        //console.log('City name =>', data.city_name)
-                        result = data.city_name
-                        break
-                    }
-                }
-            })
-            .catch(err => {
-                console.log('Error adding documents', err)
-            })
-
-        console.log('City name => ', result)
-
-        return result
+        this.cities = require('./Cities')
+        this.Cities = new this.cities()
     }
 
     async getDust(city_id, token, callback) {
@@ -58,7 +34,7 @@ module.exports = class Dust {
                                 console.log('Dust data =>', data)
                                 cityData.push({
                                     city_id: data.city_id,
-                                    city_name: await this.getCityName(data.city_id),
+                                    city_name: await this.Cities.getCityName(data.city_id),
                                     times: data.times
                                 })
                                 break
@@ -81,7 +57,7 @@ module.exports = class Dust {
                                 //console.log('Dust data =>', data)
                                 cityData.push({
                                     city_id: data.city_id,
-                                    city_name: await this.getCityName(data.city_id),
+                                    city_name: await this.Cities.getCityName(data.city_id),
                                     times: data.times
                                 })
                             }
@@ -95,7 +71,7 @@ module.exports = class Dust {
                                     if (data.city_id === city_id) {
                                         cityData.push({
                                             city_id: data.city_id,
-                                            city_name: await this.getCityName(data.city_id),
+                                            city_name: await this.Cities.getCityName(data.city_id),
                                             times: data.times
                                         })
                                         break
