8e20295c8e80bc1e16fb803df117bac52a3e2359
mduccc <duc2521997@gmail.com>
2019-09-07 23:59:41 +0700

    Hash Password

--- App.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-const server = require('./Server')
-const app = new server().start()
-const checkpoint = require('./middleware/CheckPoint')
-
-new checkpoint(app).go()
--- business/v1/StringRandom.js
+++
@@ -1,9 +0,0 @@
-module.exports = function random(length) {
-    let result = ''
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    let charactersLength = characters.length
-    for (let i=0; i<length; i++) 
-        result += characters.charAt(Math.floor(Math.random() * charactersLength))
-
-    return result
-}
